c2b9dd772b0a9b6b0fc35012a4f4db5930d970a8
redone <elkaboussi@pm.me>
2022-01-21 21:53:47 +0100

    replace regular code with functions

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 
+def get_titles():
+    title = []
+    for i in range(2, len(size)+2):
+        temp = soup.select(f"div.tgxtablerow:nth-child({i}) > div:nth-child(4) > div:nth-child(1) > a:nth-child(1) > b:nth-child(1)")
+        if temp != []:
+            for i in temp:
+                i = str(i)
+                removal_list = ["<b>","</b>"]
+                edit_string_as_list = i.split("<b>")
+                final_list = [word for word in edit_string_as_list if word not in removal_list]
+                i = ' '.join(final_list)
+                edit_string_as_list = i.split("</b>")
+                final_list = [word for word in edit_string_as_list if word not in removal_list]
+                i = ' '.join(final_list)
+                title.append(i)
+    return title
+
+def get_magnet_links():
+    '''this function search for the magnet links'''
+    magnet = []
+    for item in magnets:
+        link = item.get('href')
+        if "magnet" in str(link):
+            magnet.append(link)
+    return magnet
+
 search = input("What are you searching for? ").split(" ")
 search = "+".join(search)
 
@@ -17,33 +43,18 @@ magnets = soup.find_all("a")
 
 size = [ item.text for item in sizes ]
 
-title = []
-for i in range(2, len(size)+2):
-    temp = soup.select(f"div.tgxtablerow:nth-child({i}) > div:nth-child(4) > div:nth-child(1) > a:nth-child(1) > b:nth-child(1)")
-    if temp != []:
-        for i in temp:
-            i = str(i)
-            removal_list = ["<b>","</b>"]
-            edit_string_as_list = i.split("<b>")
-            final_list = [word for word in edit_string_as_list if word not in removal_list]
-            i = ' '.join(final_list)
-            edit_string_as_list = i.split("</b>")
-            final_list = [word for word in edit_string_as_list if word not in removal_list]
-            i = ' '.join(final_list)
-            title.append(i)
+
+title = get_titles()
 
 
-magnet = []
-for item in magnets:
-    link = item.get('href')
-    if "magnet" in str(link):
-        magnet.append(link)
 
-    data = {
+magnet = get_magnet_links()
+
+data = {
     "Title": title,
     "size": size,
     "Link": magnet
-}    
+}
 
 df = pandas.DataFrame(data)
 print(df)
