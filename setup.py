7d380e8114f8d12077f2a83da38454828c7c8f51
redouane <95357302+red-elka@users.noreply.github.com>
2022-02-06 01:15:48 +0100

    change Beautifulsoup to bs4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
    name='stream-cli',
    version='0.1',
-   install_requires=["requests", "BeautifulSoup4", "prettytable"], 
+   install_requires=["requests", "bs4", "prettytable"], 
    scripts=['stream-cli']
 )
 
