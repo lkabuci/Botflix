38cb45b81ddf5b6661092783654b60b7bbabc70d
Aymane11 <aymaneboumaaza@gmail.com>
2022-05-07 18:13:40 +0000

    Add exit option and keep asking for user input till valid

--- src/stream.py
+++ src/stream.py
@@ -9,18 +9,23 @@ console = Console()
 def get_magnet(magnets: List[str]) -> str:
     """takes magnets list, return the chosen magnet"""
 
-    is_valid = True
-    while is_valid:
-        number = int(console.input("[bold i]Enter Your Choice: [bold i]"))
-        if (number > len(magnets)) or (number <= 0):
-            print("\033[91m Invalid choice. \033[0m")
-        else:
-            is_valid = False
-
-    # actual index starts 0 --> 13
-    # but the table shows 1 --> 14
-    # -1 to select from 1 --> 14
-    return magnets[number - 1]
+    number = -1
+    magnet_count = len(magnets)
+    while (number < 0) or (number > magnet_count):
+        try:
+            number = int(console.input("[bold i]Enter Your Choice (or 0 if you want to quit): [bold i]"))
+            if (number < 0) or (number > magnet_count):
+                console.print(f"[red]Your choice must be between 0 (quit) and {magnet_count}.[red]")
+        except Exception:
+            console.print("[red]Invalid input.[red]")
+    if number == 0:
+        console.print("[red]Quitting...[red]")
+        exit()
+    else:
+        return magnets[number - 1]
+        # actual index starts 0 --> 13
+        # but the table shows 1 --> 14
+        # -1 to select from 1 --> 14
 
 
 def stream(magnet: str, default_player: str) -> None:
