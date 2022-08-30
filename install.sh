6008d1cf1ec0d97e71f3ec867a3aaa4283d0fb43
Redone <elkaboussi@pm.me>
2022-08-30 12:55:53 +0000

    add installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+READ='\e[31m%s\e[0m'
+GREEN='\e[32m%s\e[0m'
+YELLOW='\e[33m%s\e[0m'
+BLUE='\e[34m%s\e[0m'
+
+sudo npm install webtorrent-cli -g
+python -m venv venv
+source venv/bin/activate
+clear
+
+#printf "Green" "Successfully created a VE"
+echo -n mpv > player.txt
+printf $BLUE "mpv is now your default player change it latter"
+
+# Create an executable file
+mainpath=$PWD
+echo """
+cd $mainpath
+source venv/bin/activate
+python main.py
+""" > botflix
+chmod +x botflix
+
+# move the executable file to path
+sudo cp botflix /usr/bin/botflix
